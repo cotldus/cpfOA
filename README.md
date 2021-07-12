97546ecf1b5f65090f86a463f86031fb10448243
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-12 15:47:26 +0800

    upload code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cpfOA
 Simple CPFOA calculator given salary and years
+
+# Run
+from command line run
+```python3 cpfOA.py```
+
+#Assumptions
+CPF OA 
+- personal contribution of 20%
+- employer contribution of 3%
+
+CPF OA interest rate at 2.5%
+
+30 days in a month.
+
+#Note
+
+Salary allows floating number.
+Years take in whole number.
+
---
+++ cpfOA.py
@@ -0,0 +1,16 @@
+def cpf(salary, years):
+    contribution = salary * 0.23
+    i = 0
+    savings = 0
+    while i < years:
+        a = 0
+        i+=1
+        while a<13:
+            savings = (savings + contribution) * (1+ 0.025/365*30)
+            a+=1
+    print('$'+'{:10,.2f}'.format(savings))
+
+if __name__ == '__main__':
+    salary = float(input('Salary: '))
+    years = int(input('Years: '))
+    cpf(salary, years)
